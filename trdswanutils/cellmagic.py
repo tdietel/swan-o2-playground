1a77a61a195563a7d8dfb4f902df7eebae24659a
Tom Dietel <tom@dietel.net>
2022-04-30 03:17:48 +0200

    Convert genfile cell magic to jinja2

--- trdswanutils/cellmagic.py
+++ trdswanutils/cellmagic.py
@@ -1,51 +1,43 @@
-from IPython.core.magic import register_cell_magic
-import string
+from IPython.core.magic import register_cell_magic, no_var_expand
+# import string
 # import re
 import argparse
 import pathlib
-
-
-class NamespaceFormatter(string.Formatter):
-   def __init__(self, namespace={}):
-       string.Formatter.__init__(self)
-       self.namespace = namespace
-
-   def get_value(self, key, args, kwds):
-       if isinstance(key, str):
-           try:
-               # Check explicitly passed arguments first
-               return kwds[key]
-           except KeyError:
-               return self.namespace[key]
-       else:
-           string.Formatter.get_value(key, args, kwds)
+import jinja2
 
 
 @register_cell_magic
+@no_var_expand
 def genfile(line, cell):
     """ Generate file 
     
     This magic is an extension of the %%writefile magic. The additional functionality 
     includes:
-      - variable expansion (similar to string.format and f-strings)
+      - variable expansion wih Jinja2
       - set file permisssions with -m | --mode flag"""
 
-    # parse the command line arguments
+    # PARSE COMMAND LINE ARGUMENTS
     parser = argparse.ArgumentParser(description='Generate file magic')
     parser.add_argument('outfile', type=str, help='output file')
     parser.add_argument('-m', '--mode', help='file permissions')
     args = parser.parse_args(line.split())
 
-    # format the content
-    fmt = NamespaceFormatter(globals())
-    content = fmt.format(cell)
+    # FORMAT THE CONTENT
+    
+    # We ask get_python() to give us all globals with ev("globals()"), which 
+    # we then provide to the Jinja2 template
+    content = jinja2.Template(cell).render(**get_ipython().ev("globals()"))
+    
+    # Replace leading spaces with tabs
+    # TODO: it would be better to make this configurable with
+    # an argparse option
     content = content.replace("\n    ", "\n\t")
 
     # TODO: this should check if the file content changed to
     # prevent updating the timestamp if nothing changed.
     # This will help with Makefile dependencies.
 
-    # generate the file
+    # GENERATE THE FILE    
     filename = pathlib.Path(args.outfile)
     with open(filename, "w") as out:
         out.write(content)
