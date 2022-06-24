fa6359d124524220183c4256083047ce2e94da8a
Tom Dietel <tom@dietel.net>
2022-06-24 13:42:41 +0200

    Implement digit,tracklet display
    Digits and tracklets can now be drawn with a ROOT macro in rawdisplay.
    The DataManager was modified to facilitate looping over digits and
    tracklets by detector/padrow/MCM. The Jupyter notebooks in Noise/ were
    updated to use the modified DataManager.

--- Noise/DataManager.C
+++ Noise/DataManager.C
@@ -1,18 +1,185 @@
 
-// #include <TPython.h>
+#include <DataFormatsTRD/Digit.h>
+#include <DataFormatsTRD/Tracklet64.h>
+#include <DataFormatsTRD/TriggerRecord.h>
+#include <DataFormatsTRD/Hit.h>
+#include <DataFormatsTRD/Constants.h>
 
-int DigitIdx(const o2::trd::Digit& a)
+#include <DataFormatsTPC/TrackTPC.h>
+
+#include <TSystem.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TTreeReader.h>
+#include <TTreeReaderArray.h>
+
+
+template <typename value_t, template <typename T> typename container_t>
+struct myrange
 {
-  int ret = a.getDetector();
-  ret *= 1000; ret += a.getPadRow();
-  ret *= 1000; ret += a.getPadCol();
-  return ret;
+  // typedef value_t value_type;
+  typedef typename container_t<value_t>::iterator iterator;
+
+  iterator &begin() { return b; }
+  iterator &end() { return e; }
+  iterator b, e;
+
+  size_t length() { return e - b; }
+  void sort(bool (*comp)(const value_t &a, const value_t &b))
+  {
+    std::stable_sort(b, e, comp);
+  }
+};
+
+struct RawDataSpan
+{
+  myrange<o2::trd::Digit, TTreeReaderArray> digits;
+  myrange<o2::trd::Tracklet64, TTreeReaderArray> tracklets;
+};
+
+ostream& operator<<(ostream& os, RawDataSpan& sp)
+{
+  os << "raw data span with " << sp.digits.length() << " digits and " 
+     << sp.tracklets.length() << " tracklets";
+  return os;
 }
 
-bool DigitCompare(const o2::trd::Digit& a, const o2::trd::Digit& b)
+bool order_digit(const o2::trd::Digit &a, const o2::trd::Digit &b)
 {
-  return DigitIdx(a) < DigitIdx(b);
+  if (a.getDetector() != b.getDetector())
+    return a.getDetector() < b.getDetector();
+
+  if (a.getPadRow() != b.getPadRow())
+    return a.getPadRow() < b.getPadRow();
+
+  if (a.getROB() != b.getROB())
+    return a.getROB() < b.getROB();
+
+  if (a.getMCM() != b.getMCM())
+    return a.getMCM() < b.getMCM();
+
+  if (a.getChannel() != b.getChannel())
+    return a.getChannel() < b.getChannel();
+
+  return true;
 }
+
+bool order_tracklet(const o2::trd::Tracklet64 &a, const o2::trd::Tracklet64 &b)
+{
+  // upper bits of hcid and padrow from Tracklet64 word
+  const uint64_t det_row_mask = 0x0ffde00000000000;
+
+  // lowest bit of hcid (side), MCM col and pos from Tracklet64 word
+  const uint64_t col_pos_mask = 0x00011fff00000000;
+
+  auto a_det_row = a.getTrackletWord() & det_row_mask;
+  auto b_det_row = b.getTrackletWord() & det_row_mask;
+
+  if (a_det_row != b_det_row)
+    return a_det_row < b_det_row;
+
+  auto a_col_pos = a.getTrackletWord() & col_pos_mask;
+  auto b_col_pos = b.getTrackletWord() & col_pos_mask;
+
+  return a_col_pos < b_col_pos;  
+};
+
+
+struct ClassifierByDetector
+{
+  static uint32_t key(const o2::trd::Digit &x) { return x.getDetector(); }
+  static uint32_t key(const o2::trd::Tracklet64 &x) { return x.getDetector(); }
+
+  static bool comp_digits(const o2::trd::Digit &a, const o2::trd::Digit &b)
+  {
+    return key(a) != key(b);
+  }
+
+  static bool comp_tracklets(const o2::trd::Tracklet64 &a, const o2::trd::Tracklet64 &b)
+  {
+    return key(a) != key(b);
+  }
+};
+
+struct ClassifierByPadRow
+{
+  static uint32_t key(const o2::trd::Digit &x) { 
+    return 100*x.getDetector() + x.getPadRow();
+  }
+  static uint32_t key(const o2::trd::Tracklet64 &x) {
+    return 100 * x.getDetector() + x.getPadRow();
+  }
+
+  static bool comp_digits(const o2::trd::Digit &a, const o2::trd::Digit &b)
+  {
+    return key(a) != key(b);
+  }
+
+  static bool comp_tracklets(const o2::trd::Tracklet64 &a, const o2::trd::Tracklet64 &b)
+  {
+    return key(a) != key(b);
+  }
+};
+
+struct ClassifierByMCM
+{
+  template<typename T>
+  static uint32_t key(const T &x) { 
+    return 1000*x.getDetector() + 10*x.getPadRow() + 4*(x.getROB()%2) + x.getMCM()%4;
+  }
+
+  static bool comp_digits(const o2::trd::Digit &a, const o2::trd::Digit &b)
+  {
+    return key(a) != key(b);
+  }
+
+  static bool comp_tracklets(const o2::trd::Tracklet64 &a, const o2::trd::Tracklet64 &b)
+  {
+    return key(a) != key(b);
+  }
+};
+
+
+template<typename classifier>
+class RawDataPartitioner : public map<uint32_t, RawDataSpan>
+{
+public:
+
+  RawDataPartitioner(RawDataSpan event)
+  {
+    // sort digits and tracklets
+    event.digits.sort(order_digit);
+    event.tracklets.sort(order_tracklet);
+
+    // add all the digits to a map that contains all the 
+    for (auto cur = event.digits.b; cur != event.digits.e; /* noop */ ) {
+      // let's look for pairs where the comparision function is true, i.e.
+      // where two adjacent elements do not have equal keys
+      auto nxt = std::adjacent_find(cur, event.digits.e, classifier::comp_digits);
+      // if adjacent_find found a match, it returns the first element of the
+      // adjacent pair, but we need the second one -> increment the result
+      if (nxt != event.digits.e)
+      {
+        ++nxt;
+      }
+      // finally, we add the found elements to the map with the current range
+      (*this)[classifier::key(*cur)].digits.b = cur; 
+      (*this)[classifier::key(*cur)].digits.e = nxt;
+      cur = nxt;
+    }
+
+    for (auto cur = event.tracklets.b; cur != event.tracklets.e; /* noop */) {
+      auto nxt = std::adjacent_find(cur, event.tracklets.e, classifier::comp_tracklets);
+      if (nxt != event.tracklets.e) {
+        ++nxt;
+      }
+      (*this)[classifier::key(*cur)].tracklets.b = cur; 
+      (*this)[classifier::key(*cur)].tracklets.e = nxt;
+      cur = nxt;
+    }
+
+  }
+};
 
 
 class DataManager
@@ -23,7 +190,7 @@ public:
   DataManager(std::string dir="data/")
   : mainfile(0), datatree(0), datareader(0),
     hits(0), digits(0), tracklets(0), trgrecords(0),
-    tfno(0), evno(0), iEvent(-1)
+    iTimeframe(0), iEvent(0)
   {
 
     std::string testfiles[] = { "trddigits.root", "trdtracklets.root" };
@@ -44,31 +211,40 @@ public:
         datatree->AddFriend("o2sim", fullname.c_str());
       }
     }
-    
+
+    // auto tracksfilename = dir + "tpctracks.root";
+    // if (gSystem->AccessPathName(tracksfilename.c_str())) {
+    //   datatree->AddFriend("tpcrec", tracksfilename.c_str());
+    // }
+     
     // set up the branches we want to read
-    digits     = new TTreeReaderArray<o2::trd::Digit>(*datareader, "TRDDigit");
-    tracklets  = new TTreeReaderArray<o2::trd::Tracklet64>(*datareader, "Tracklet");
+    digits = new TTreeReaderArray<o2::trd::Digit>(*datareader, "TRDDigit");
+    tracklets = new TTreeReaderArray<o2::trd::Tracklet64>(*datareader, "Tracklet");
     trgrecords = new TTreeReaderArray<o2::trd::TriggerRecord>(*datareader, "TrackTrg");
+    // tpctracks = new TTreeReaderArray<o2::tpc::TrackTPC>(*datareader, "TPCTracks");
 
     // ConnectMCHitsFile(dir+"o2sim_HitsTRD.root");
-
-  }
+    }
 
   bool NextTimeFrame()
   {
     if (datareader->Next() ) {
-
-      cout << "## Time frame " << tfno << endl;
-
       iEvent = 0;
-      evno = 0;
-      tfno++;
-      return true;
+      iTimeframe++;
+      cout << "## Time frame " << iTimeframe << endl;
 
+      for (auto &tracklet : *tracklets)
+      {
+        tracklet.setPosition(tracklet.getPosition() ^ 0x80);
+        tracklet.setSlope(tracklet.getSlope() ^ 0x80);
+      }
+
+      // cout << "Found " << tpctracks->GetSize() << " TPC tracks" << endl;
+
+      return true;
     } else {
       return false;
     }
-
   }
 
   bool NextEvent()
@@ -80,101 +256,57 @@ public:
 
     // load the hits for the next event
     // if ( ! rdrhits->Next() ) {
-    //   cout << "no hits found for event " << tfno << ":" << evno << endl;
+    //   cout << "no hits found for event " << tfno << ":" << iEvent << endl;
     //   return false;
     // }
 
-    auto evrec = trgrecords->At(iEvent);
-    cout << evrec << endl;
+    mTriggerRecord = trgrecords->At(iEvent);
+    // cout << mTriggerRecord << endl;
 
-    cout << "## Event " << tfno << ":" << evno << ":  "
+    cout << "## Event " << iTimeframe << ":" << iEvent << ":  "
         //  << hits->GetSize() << " hits   "
-         << evrec.getNumberOfDigits() << " digits" << endl;
+         << mTriggerRecord.getNumberOfDigits() << " digits and "
+         << mTriggerRecord.getNumberOfTracklets() << " tracklets" << endl;
 
-    event_digits.b = digits->begin() + evrec.getFirstDigit();
-    event_digits.e = event_digits.begin() + evrec.getNumberOfDigits();
-    std::stable_sort(event_digits.b,event_digits.e,DigitCompare);
-
-    // event_hits.b  = hits->begin();
-    // event_hits.e  = hits->end();
-
-    evno++;
     iEvent++;
     return true;
   }
 
-  // o2::trd::TriggerRecord GetTriggerRecord() {return trgrecords->At(iEvent);}
+  RawDataSpan GetEvent()
+  {
+    RawDataSpan ev;
+    // ev.digits = GetDigits();
+    ev.digits.b = digits->begin() + mTriggerRecord.getFirstDigit();
+    ev.digits.e = ev.digits.begin() + mTriggerRecord.getNumberOfDigits();
+    ev.tracklets.b = tracklets->begin() + mTriggerRecord.getFirstTracklet();
+    ev.tracklets.e = ev.tracklets.begin() + mTriggerRecord.getNumberOfTracklets();
+    return ev;
+  }
+
+  size_t GetTimeFrameNumber() { return iTimeframe; }
+  size_t GetEventNumber() { return iEvent; }
 
 
+protected :
+      // void ConnectMCHitsFile(std::string fname)
+      // {
+      //   // ----------------------------------------------------------------------
+      //   // set up data structures for reading
 
-  // A struct to be used for range-based for loops
-  // A template would be nice, but I did not manage to make it work.
-  struct DigitRange {
-    TTreeReaderArray<o2::trd::Digit>::iterator& begin() { return b; }
-    TTreeReaderArray<o2::trd::Digit>::iterator& end() { return e; }
-    TTreeReaderArray<o2::trd::Digit>::iterator b, e, c;
+      //   if (fhits || trhits) {
+      //     cerr << "Hits file seems to be connected." << endl;
+      //     return;
+      //   }
 
-    /// I had the idea to implement the iterator in C++, but didn't manage
-    // DigitRange* __iter__() { c=b; return this; }
-    // o2::trd::Digit __next__() {
-    //   if (c==e) {
-    //     TPython.Eval("raise StopIteration");
-    //   } else {
-    //     return *(c++);
-    //   }
-    // }
-  };
+      //   fhits = new TFile(fname.c_str());
+      //   fhits->GetObject("o2sim", trhits);
 
+      //   rdrhits = new TTreeReader(trhits);
+      //   hits = new TTreeReaderArray<o2::trd::Hit>(*rdrhits, "TRDHit");
+      // }
 
-  DigitRange event_digits, padrow_digits;
-  DigitRange Digits() {return event_digits;}
-
-  // A struct to be used for range-based for loops
-  // A template would be nice, but I did not manage to make it work.
-  struct HitRange {
-    TTreeReaderArray<o2::trd::Hit>::iterator& begin() { return b; }
-    TTreeReaderArray<o2::trd::Hit>::iterator& end() { return e; }
-    TTreeReaderArray<o2::trd::Hit>::iterator b, e;
-   };
-
-   HitRange event_hits;
-   HitRange Hits() {return event_hits;}
-
-
-protected:
-  // void ConnectMCHitsFile(std::string fname)
-  // {
-  //   // ----------------------------------------------------------------------
-  //   // set up data structures for reading
-
-  //   if (fhits || trhits) {
-  //     cerr << "Hits file seems to be connected." << endl;
-  //     return;
-  //   }
-
-  //   fhits = new TFile(fname.c_str());
-  //   fhits->GetObject("o2sim", trhits);
-
-  //   rdrhits = new TTreeReader(trhits);
-  //   hits = new TTreeReaderArray<o2::trd::Hit>(*rdrhits, "TRDHit");
-  // }
-
-  // void ConnectDigitsFile(std::string fname)
-  // {
-
-  //   fdigits = new TFile(fname.c_str());
-  //   fdigits->GetObject("o2sim", trdigits);
-
-  //   rdrdigits = new TTreeReader(trdigits);
-
-  //   // set up the branches we want to read
-  //   digits = new TTreeReaderArray<o2::trd::Digit>(*rdrdigits, "TRDDigit");
-  //   trgrecords = new TTreeReaderArray<o2::trd::TriggerRecord>(*rdrdigits, "TriggerRecord");
-  // }
-
-private:
-  // std::list<TFile*> files;
-  TFile* mainfile;
+private: 
+  TFile *mainfile;
   // TFile *fhits, *fdigits, *ftracklets;
   // TTree *trhits, *trdigits, *trtracklets, *trtrgrec;
   TTree* datatree; // tree and friends from digits, tracklets files
@@ -186,5 +318,12 @@ private:
   TTreeReaderArray<o2::trd::Tracklet64>* tracklets;
   TTreeReaderArray<o2::trd::TriggerRecord>* trgrecords;
 
-  int tfno, evno, iEvent;
+  TTreeReaderArray<o2::tpc::TrackTPC> *tpctracks;
+
+  // DigitRange ev_digits;//, sel_digits;
+  o2::trd::TriggerRecord mTriggerRecord;
+
+  size_t iTimeframe, iEvent;
 };
+
+
